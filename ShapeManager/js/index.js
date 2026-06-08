c102d8dd643871cfc5ea4f4f94f8d1de4f6765f4
Yerbossyn <erbosynmagzan@gmail.com>
2026-06-08 15:18:13 +0500

    Добавил шаблон сайт с HTML & CSS, подключил js файл от ts

---
+++ ShapeManager/js/index.js
@@ -0,0 +1,4 @@
+const resultCardElement = document.getElementById('result-card');
+const resultTextElement = document.getElementById('result-text');
+resultCardElement.classList.add('visible');
+export {};
