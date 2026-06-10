24265906803c7f5a5b7a28815efe9cc8394211d7
Yerbossyn <erbosynmagzan@gmail.com>
2026-06-10 23:36:54 +0500

    дополнение к generics

--- Theory/constraints.ts
+++ Theory/constraints.ts
@@ -19,3 +19,12 @@ const notOK = longest(10, 100);
 
 // Argument of type 'number' is not assignable to parameter of type '{ length: number }'.
 // Аргумент типа 'number' не может быть присвоен параметру типа '{ length: number; }'
+
+function combine<Type>(arr1: Type[], arr2: Type[]): Type[] {
+  return arr1.concat(arr2);
+}
+
+const arr1 = combine([1, 2, 3], ['привет']);
+// Type 'string' is not assignable to type 'number'.
+
+const arr2 = combine<string | number>([1, 2, 3], ['привет']);
